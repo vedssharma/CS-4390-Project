0a8893e94007f57efc2d79118ed705f521913e89
Ved Sharma <vedssharma@gmail.com>
2023-11-19 22:00:59 -0600

    add client and server classes

---
+++ src/Client.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.net.*;
+
+public class Client {
+
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,6 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    public static void main(String[] args){}
+}
