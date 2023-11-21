f614ec5b001fbd597b976efce9907d8569d29fee
Ved Sharma <vedssharma@gmail.com>
2023-11-20 20:14:33 -0600

    Added basic structure for sockets and streams

--- src/Client.java
+++ src/Client.java
@@ -3,7 +3,33 @@ import java.net.*;
 
 public class Client {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception{
+        String expression;
+        String result;
+
+        // Client input stream
+        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(System.in));
+
+        // Client socket
+        Socket clientSocket = new Socket("localhost", 6789);
+
+        // Client output stream
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
+        // Input stream from server
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        // Read input from client
+        expression = inFromClient.readLine();
+
+        // Send input to server
+        outToServer.writeBytes(expression + "\n");
+
+        // Read result from server and print it
+        result = inFromServer.readLine();
+        System.out.println("Server: " + result);
+
+        clientSocket.close();
 
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -2,5 +2,31 @@ import java.io.*;
 import java.net.*;
 
 public class Server {
-    public static void main(String[] args){}
+    public static void main(String[] args) throws Exception{
+        String clientExpression;
+        String result;
+
+        // Server socket
+        ServerSocket welcomeSocket = new ServerSocket(6789);
+
+        while(true){
+            // Accept client connection
+            Socket clientSocket = welcomeSocket.accept();
+
+            // Client input stream
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            // Output stream to client
+            DataOutputStream outToClient = new DataOutputStream(clientSocket.getOutputStream());
+
+            // Read expression from client
+            clientExpression = inFromClient.readLine();
+
+            //Calculate the result of the expression
+
+            // Send result to client
+
+        }
+
+    }
 }
